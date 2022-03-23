ca26d5838b7abd132de51ce305815a451fb682f3
fresh-professor <c16439@gmail.com>
2022-03-23 16:34:30 +0900

    .

--- models/DiverseCont.py
+++ models/DiverseCont.py
@@ -21,28 +21,14 @@ class DiverseCont(torch.nn.Module):
         self.device = config['device']
         self.writer = writer
 
-        self.purified_buffer = reservoir['purified'](config, config['purified_buffer_size'], config['purified_buffer_q_poa'])
+        self.diverse_buffer = reservoir['diverse'](config, config['diverse_buffer_size'], config['diverse_buffer_q_poa'])
         self.delay_buffer = reservoir['delay'](config, config['delayed_buffer_size'], config['delayed_buffer_q_poa'])
-
-        self.E_max = config['E_max']
 
         self.expert_step = 0
         self.base_step = 0
         self.base_ft_step = 0
 
         self.base = self.get_init_base(config)
-
-        #self.expert_number = 0
-        #self.expert = self.get_init_expert(config)
-
-        self.ssl_dir = os.path.join(os.path.dirname(os.path.dirname(self.config['log_dir'])),
-                                    'noiserate_{}'.format(config['corruption_percent']),
-                                    'expt_{}'.format(config['expert_train_epochs']),
-                                    'randomseed_{}'.format(config['random_seed']))
-
-        if os.path.exists(self.ssl_dir):
-            with open(os.path.join(self.ssl_dir, 'idx_sets.npy'), 'rb') as f:
-                self.debug_idxs = np.load(f, allow_pickle=True)
 
     def get_init_base(self, config):
         """get initialized base model"""
@@ -54,18 +40,7 @@ class DiverseCont(torch.nn.Module):
         base.setup_optimizer(optim_config)
         base.setup_lr_scheduler(lr_scheduler_config)
         return base
-    '''
-    def get_init_expert(self, config):
-        """get initialized expert model"""
-        expert = Net[config['net']](config)
-        optim_config = config['optimizer']
-        lr_scheduler_config = deepcopy(config['lr_scheduler'])
-        lr_scheduler_config['options'].update({'T_max': config['expert_train_epochs']})
 
-        expert.setup_optimizer(optim_config)
-        expert.setup_lr_scheduler(lr_scheduler_config)
-        return expert
-    '''
     def get_init_base_ft(self, config):
         """get initialized eval model"""
         base_ft = Net[config['net'] + '_ft'](config)
@@ -82,26 +57,11 @@ class DiverseCont(torch.nn.Module):
             self.delay_buffer.update(imgs=x[i: i + 1], cats=y[i: i + 1], corrupts=corrupt[i: i + 1], idxs=idx[i: i + 1])
 
             if self.delay_buffer.is_full():
-                '''
-                if not os.path.exists(os.path.join(self.ssl_dir, 'model{}.ckpt'.format(self.expert_number))):
-                    self.expert = self.get_init_expert(self.config)
-                    self.train_self_expert()
-                else:
-                    self.expert.load_state_dict(
-                        torch.load(os.path.join(self.ssl_dir, 'model{}.ckpt'.format(self.expert_number)),
-                                    map_location=self.device))
-                    ################### data consistency check ######################
-                    if torch.sum(self.delay_buffer.get('idxs') != torch.Tensor(self.debug_idxs[self.expert_number])) != 0:
-                        raise Exception("it seems there is a data consistency problem: exp_num {}".format(self.expert_number))
-                    ################### data consistency check ######################
-                '''
                 self.train_self_base()
-
-                #clean_idx, clean_p = self.cluster_and_sample()
+                diverse_idx, clean_p = self.cluster_and_sample()
                 #self.update_purified_buffer(clean_idx, clean_p, step)
                 imgs, cats, corrupts, idxs, clean_p = self.cluster_and_sample()
                 self.update_purified_buffer_full_info(imgs, cats, corrupts, idxs, clean_p, step)
-                #self.expert_number += 1
 
     def update_purified_buffer(self, clean_idx, clean_p, step):
         """
@@ -136,6 +96,7 @@ class DiverseCont(torch.nn.Module):
         print(colorful.bold_yellow(self.purified_buffer.state('corrupts')).styled_string)
         self.writer.add_scalar(
             'buffer_corrupts', torch.sum(self.purified_buffer.get('corrupts')), step)
+
     def cluster_and_sample(self):
         """filter samples in delay buffer"""
         self.base.eval()
@@ -145,46 +106,52 @@ class DiverseCont(torch.nn.Module):
             corrs = self.delay_buffer.get('corrupts')
             idxs = self.delay_buffer.get('idxs')
 
-            if self.purified_buffer.is_full():
-                pb_xs = self.purified_buffer.get('imgs')
-                pb_ys = self.purified_buffer.get('cats')
-                pb_corrs = self.purified_buffer.get('corrupts')
-                pb_idxs = self.purified_buffer.get('idxs')
+            # merge delay_buffer and diverse_buffer
+            if self.diverse_buffer.is_full():
+                divb_xs = self.diverse_buffer.get('imgs')
+                divb_ys = self.diverse_buffer.get('cats')
+                divb_corrs = self.diverse_buffer.get('corrupts')
+                divb_idxs = self.diverse_buffer.get('idxs')
 
-                xs = torch.cat((xs, pb_xs), dim=0)
-                ys = torch.cat((ys, pb_ys), dim=0)
-                corrs = torch.cat((corrs, pb_corrs), dim=0)
-                idxs = torch.cat((idxs, pb_idxs), dim=0)
+                xs = torch.cat((xs, divb_xs), dim=0)
+                ys = torch.cat((ys, divb_ys), dim=0)
+                corrs = torch.cat((corrs, divb_corrs), dim=0)
+                idxs = torch.cat((idxs, divb_idxs), dim=0)
 
             features = self.base(xs)
             features = F.normalize(features, dim=1)
 
             clean_p = list()
             clean_idx = list()
+
             imgs = torch.Tensor().cuda()
             cats = torch.Tensor().cuda()
             corrupts = torch.Tensor()
             indexs = torch.Tensor()
-            print("***********************************************")
+            print("*" * 30)
             # Find the proper number of y class
-            pb_size = self.purified_buffer.rsvr_total_size
+            divb_size = self.diverse_buffer.rsvr_total_size
 
             # sorting unique_ys based on its counts
             unique_ys, unique_ys_count= torch.unique(ys, return_counts = True)
             unique_ys = unique_ys[torch.sort(unique_ys_count)[1]]
             unique_ys_count = torch.sort(unique_ys_count)[0]
 
-            # get the proper_size
-            proper_size = int(pb_size / unique_ys.shape[0])
-            print(f"Proper_size = {proper_size}")
+            # get the proper_size (balanaced sampling, diverse_buffer.size/num_class)
+            proper_size = int(divb_size / unique_ys.shape[0])
+            print(f"Proper_size per class = {proper_size}")
 
-            #for u_y in torch.unique(ys).tolist():
+            print(f"Start clustering per class")
             for u_y, u_y_c in zip(unique_ys, unique_ys_count):
-                print(f"unique y / unique y count : {u_y} / {u_y_c}")
+                print(f"class {u_y}, count {u_y_c}")
                 y_mask = (ys == u_y)
-                corr = corrs[y_mask]
 
-                print("************************************************************")
+                x = xs[y_mask]
+                y = ys[y_mask]
+                corr = corrs[y_mask]
+                idx = idxs[y_mask]
+
+                print("*" * 30)
                 if (proper_size >= u_y_c) :
                     print(f"Iterative Sample Selection (just pass dataset (not enough data))")
                     # pass all samples to purified buffer
@@ -194,81 +161,21 @@ class DiverseCont(torch.nn.Module):
                     feature = features[y_mask]
                     # ignore negative similairties
                     _similarity_matrix = torch.relu(F.cosine_similarity(feature.unsqueeze(1), feature.unsqueeze(0), dim=-1))
-
-                # stochastic ensemble
-                # _clean_ps = torch.zeros((self.E_max, len(feature)), dtype=torch.double)
-                #_clean_ps = torch.zeros(len(feature), dtype=torch.double)
-                # XXX modified version
-                # this is for stocastic process
-                # similarity_matrix = (_similarity_matrix > torch.rand_like(_similarity_matrix)).type(torch.float32)
                     similarity_matrix = _similarity_matrix.type(torch.float32)
                     similarity_matrix[similarity_matrix == 0] = 1e-5  # add small num for ensuring positive matrix
 
-                    print(f"Iterative Sample Selection (get {proper_size} from {u_y_c})")
-                    for i in tqdm.trange(proper_size, desc="iterative sampling", leave=False):
-                        g = nx.from_numpy_matrix(similarity_matrix.cpu().numpy())
-                        info = nx.eigenvector_centrality(g, max_iter=6000, weight='weight') # index: value
-                        centrality = [info[i] for i in range(len(info))]
+                    g = nx.from_numpy_matrix(similarity_matrix.cpu().numpy())
+                    info = nx.eigenvector_centrality(g, max_iter=6000, weight='weight') # index: value
+                    centrality = [info[i] for i in range(len(info))]
 
-#                get posterior for clean sample (p(z=clean|centrality))
-#                bmm_model = BetaMixture1D(max_iters=10)
-#                # fit beta mixture model
-#                c = np.asarray(centrality)
-#                c, c_min, c_max = bmm_model.outlier_remove(c)
-#                c = bmm_model.normalize(c, c_min, c_max)
-#                bmm_model.fit(c)
-#                bmm_model.create_lookup(1) # 0: noisy, 1: clean
-#
-#                # get posterior
-#                c = np.asarray(centrality)
-#                c = bmm_model.normalize(c, c_min, c_max)
-#                p = bmm_model.look_lookup(c)
-#                # this is for stocastic process
-#                # _clean_ps[_i] = torch.from_numpy(p)
-#                _clean_ps = torch.from_numpy(p)
-                        '''
-                        for _i in range(self.E_max):
-                            similarity_matrix = (_similarity_matrix > torch.rand_like(_similarity_matrix)).type(torch.float32)
-                            similarity_matrix[similarity_matrix == 0] = 1e-5  # add small num for ensuring positive matrix
+                    # get the most important node's index(m)
+                    centrality = torch.tensor(centrality)
+                    m = torch.argmax(centrality)
 
-                            g = nx.from_numpy_matrix(similarity_matrix.cpu().numpy())
-                            info = nx.eigenvector_centrality(g, max_iter=6000, weight='weight') # index: value
-                            centrality = [info[i] for i in range(len(info))]
+                    clean_idx.extend(torch.nonzero(y_mask)[:, -1][m].tolist())
+                    clean_p.extend(centrality[m].tolist())
 
-                            bmm_model = BetaMixture1D(max_iters=10)
-                            # fit beta mixture model
-                            c = np.asarray(centrality)
-                            c, c_min, c_max = bmm_model.outlier_remove(c)
-                            c = bmm_model.normalize(c, c_min, c_max)
-                            bmm_model.fit(c)
-                            bmm_model.create_lookup(1) # 0: noisy, 1: clean
-
-                            # get posterior
-                            c = np.asarray(centrality)
-                            c = bmm_model.normalize(c, c_min, c_max)
-                            p = bmm_model.look_lookup(c)
-                            _clean_ps[_i] = torch.from_numpy(p)
-                        '''
-                        #_clean_ps = torch.mean(_clean_ps, dim=0)
-#                _clean_ps = _clean_ps
-                        # This is not threshold.
-                        # This code is used for "sampling with probability"
-                        #m = _clean_ps > torch.rand_like(_clean_ps)
-
-                        # get the most important node
-                        centrality = torch.Tensor(centrality)
-                        centrality = (centrality - centrality.min())/(centrality.max() - centrality.min())
-                        m = torch.argmax(centrality)
-                        #clean_idx.extend(torch.nonzero(y_mask)[:, -1][m].tolist())
-                        #clean_p.extend(centrality[m].tolist())
-                        clean_idx.append(torch.nonzero(y_mask)[:, -1][m].tolist())
-                        clean_p.append(centrality[m].tolist())
-
-                        # Set 0 to m's edge
-                        similarity_matrix[m, :] = 1e-5
-                        similarity_matrix[:, m] = 1e-5
-
-                print("************************************************************")
+                print("*" * 30)
                 #clean_idx.extend(torch.nonzero(y_mask)[:, -1][m].tolist())
                 #clean_p.extend(_clean_ps[m].tolist())
                 #print(xs.shape)
@@ -296,24 +203,35 @@ class DiverseCont(torch.nn.Module):
         return imgs, cats, corrupts, indexs, clean_p
 
     def train_self_base(self):
-        """Self Replay. train base model with samples from delay and purified buffer"""
+        """
+        Self Replay. train base model with samples from delay and purified buffer
+        """
         bs = self.config['base_batch_size']
-        # If purified buffer is full, train using it also
-        db_bs = (bs // 2) if self.purified_buffer.is_full() else bs
+        # If diverse buffer is full, train using it also
+        # db_bs: delay buffer batch size
+        # divb_bs: diverse buffer batch size
+        db_bs = (bs // 2) if self.diverse_buffer.is_full() else bs
         db_bs = min(db_bs, len(self.delay_buffer))
-        pb_bs = min(bs - db_bs, len(self.purified_buffer))
+        divb_bs = min(bs - db_bs, len(self.diverse_buffer))
 
         self.base.train()
-        self.base.init_ntxent(self.config, batch_size=db_bs + pb_bs)
+        self.base.init_ntxent(self.config, batch_size=db_bs + divb_bs)
+
+        # Remember
+        # ***_buffer.get_dataloader(self, batch_szie, drop_last, shuffle)
+        #   -> return DataLoader
+        # ***_buffer.sample(self, num, cat=None)
+        #   -> return samples
 
         dataloader = self.delay_buffer.get_dataloader(batch_size=db_bs, shuffle=True, drop_last=True)
+
         for epoch_i in tqdm.trange(self.config['base_train_epochs'], desc="base training", leave=False):
             for inner_step, data in enumerate(dataloader):
                 x = data['imgs']
                 self.base.zero_grad()
-                # sample data from purified buffer and merge
-                if pb_bs > 0:
-                    replay_data = self.purified_buffer.sample(num=pb_bs)
+                # sample data from diverse buffer and merge
+                if divb_bs > 0:
+                    replay_data = self.diverse_buffer.sample(num=divb_bs)
                     x = torch.cat([replay_data['imgs'], x], dim=0)
 
                 loss = self.base.get_selfsup_loss(x)
@@ -330,34 +248,6 @@ class DiverseCont(torch.nn.Module):
 
         self.writer.flush()
         self.base_step += self.config['base_train_epochs'] * len(dataloader)
-
-    '''
-    def train_self_expert(self):
-        """train expert model with samples from delay"""
-        batch_size =min(self.config['expert_batch_size'], len(self.delay_buffer))
-        self.expert.train()
-        self.expert.init_ntxent(self.config, batch_size=batch_size)
-
-        dataloader = self.delay_buffer.get_dataloader(batch_size=batch_size, shuffle=True, drop_last=True)
-        for epoch_i in tqdm.trange(self.config['expert_train_epochs'], desc='expert training', leave=False):
-            for inner_step, data in enumerate(dataloader):
-                x = data['imgs']
-                self.expert.zero_grad()
-                loss = self.expert.get_selfsup_loss(x)
-                loss.backward()
-                self.expert.optimizer.step()
-
-                self.writer.add_scalar(
-                    'expert_train_loss', loss,
-                    self.expert_step + inner_step + len(dataloader) * epoch_i)
-
-            # warmup for the first 10 epochs
-            if epoch_i >= 10:
-                self.expert.lr_scheduler.step()
-
-        self.writer.flush()
-        self.expert_step += self.config['expert_train_epochs'] * len(dataloader)
-    '''
 
     def get_finetuned_model(self):
         """copy the base and fine-tune for evaluation"""
