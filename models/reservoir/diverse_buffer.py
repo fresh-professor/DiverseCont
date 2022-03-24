e07644eda51699c84e6ee0492e217561d2d6c0a7
fresh-professor <c16439@gmail.com>
2022-03-24 10:21:11 +0900

    purified_buffer -> diverse_buffer

--- models/reservoir/diverse_buffer.py
+++ models/reservoir/diverse_buffer.py
@@ -3,20 +3,6 @@ import numpy as np
 
 
 class DiverseBuffer(PRS):
-    def sample_out(self):
-        """
-        select a sample and remove from rsvr
-        """
-        probs = self.substreams.get_probs()
-        selected_key = np.random.choice(list(probs.keys()), 1, p=list(probs.values()))[0]
-        # the least confident sample is sampled first.
-        clean_weighting = []
-        for idx in self.substreams[selected_key].idxs:
-            clean_weighting.append(self.rsvr['clean_ps'][idx])
-
-        z = self.substreams[selected_key].idxs[clean_weighting.index(min(clean_weighting))]
-        self.remove_sample(z)
-
     def state(self, key):
         """override state function
         :param key: data names
